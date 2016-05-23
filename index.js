2b063f60d82e72f4f9ebe08461d2ab3e813cab28
rhiday <shahriar.rhiday@gmail.com>
2016-05-24 01:20:10 +0600

    Update index.js

--- index.js
+++ index.js
@@ -26,26 +26,22 @@ app.get('/webhook/', function (req, res) {
 
 // to post data
 app.post('/webhook/', function (req, res) {
-	messaging_events = req.body.entry[0].messaging
-	for (i = 0; i < messaging_events.length; i++) {
-		event = req.body.entry[0].messaging[i]
-		sender = event.sender.id
-		if (event.message && event.message.text) {
-			text = event.message.text
-			if (text === 'Generic') {
-				sendGenericMessage(sender)
-				continue
-			}
-			sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
-		}
-		if (event.postback) {
-			text = JSON.stringify(event.postback)
-			sendTextMessage(sender, "Postback received: "+text.substring(0, 200), token)
-			continue
-		}
-	}
-	res.sendStatus(200)
+    messaging_events = req.body.entry[0].messaging
+    for (i = 0; i < messaging_events.length; i++) {
+        event = req.body.entry[0].messaging[i]
+        sender = event.sender.id
+        if (event.message && event.message.text) {
+            text = event.message.text
+            if (text === 'Generic') {
+                sendGenericMessage(sender)
+                continue
+            }
+            sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
+        }
+    }
+    res.sendStatus(200)
 })
+
 
 var token = "EAAQy3m2Gf18BAPntPHbMia7UTQuSImRsdrZCFya91EhKWa3I3OeKNwNpq4CTgUGXSVrvq0fQUuw0GZAhe8B7jOfcgAgi77wG8HnWZCfBoAE9zjrjWE47PruyYRjtC5ZBNZBdhvc2AFgMb1RcjuEbyovswtCQMgomZB26MhFAJm2QZDZD"
 
